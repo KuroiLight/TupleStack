bbe3b54a167d474fa155bd2b046aa7d03b31047e
J J <klomb@tuta.io>
2020-11-03 15:22:36 -0800

    Add project files.

---
+++ TupleStack/TupleStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TupleStack
+{
+    //A simple implementation of a stack using C#'s new TupleValues
+    public class TupleStack<T>
+    {
+        private (object, T) data;
+        public int Depth { get; private set; }
+
+        public TupleStack()
+        {
+            data = (null, default(T));
+            Depth = 0;
+        }
+
+        public void Push(T item)
+        {
+            data = (data, item);
+            Depth++;
+        }
+
+        public T Pop()
+        {
+            if (data.Item1 == null)
+            {
+                throw new NullReferenceException("Stack empty");
+            }
+
+            T item = data.Item2;
+            data = ((object, T))data.Item1;
+            Depth--;
+            return item;
+        }
+
+        public T Peek()
+        {
+            return data.Item2;
+        }
+    }
+}
---
+++ tupletest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using TupleStack;
+
+namespace tupletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TupleStack<int> tsInt = new TupleStack<int>();
+            tsInt.Push(int.Parse(Console.ReadLine()));
+            tsInt.Push(int.Parse(Console.ReadLine()));
+            Console.WriteLine(tsInt.Pop());
+            Console.WriteLine(tsInt.Pop());
+            Console.ReadLine();
+        }
+    }
+}
